be20ca8e4316a90df2c87d0d1917b7c10fc8faee
Michael Ritter <ritter.michael92@gmail.com>
2016-03-06 16:41:03 +0100

    DCAUtil.DCAPlayer now extends Player...

--- src/main/java/net/dv8tion/jda/utils/DCAUtil.java
+++ src/main/java/net/dv8tion/jda/utils/DCAUtil.java
@@ -16,9 +16,11 @@
 package net.dv8tion.jda.utils;
 
 import net.dv8tion.jda.audio.AudioSendHandler;
+import net.dv8tion.jda.audio.player.Player;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import javax.sound.sampled.AudioInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -42,18 +44,40 @@ public class DCAUtil
         return null;
     }
 
-    private static class DCAPlayer implements AudioSendHandler {
-        private final DCAReader reader;
+    public static class DCAPlayer extends Player {
+        private  DCAReader reader;
+        private boolean started = false;
+        private boolean paused = false;
+        private boolean stopped = false;
 
         public DCAPlayer(DCAReader reader)
         {
             this.reader = reader;
         }
 
+        public DCAPlayer(File dcaFile)
+        {
+            try
+            {
+                this.reader = new DCAReader(new FileInputStream(dcaFile));
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
         @Override
         public boolean canProvide()
         {
-            return !reader.atEnd();
+            if (reader.atEnd())
+            {
+                started = false;
+                paused = false;
+                reader.close();
+                return false;
+            }
+            return started && !paused;
         }
 
         @Override
@@ -67,10 +91,81 @@ public class DCAUtil
         {
             return reader.nextPacket();
         }
+
+        @Override
+        public void play()
+        {
+            if (stopped)
+            {
+                restart();
+            }
+            started = true;
+            paused = false;
+        }
+
+        @Override
+        public void pause()
+        {
+            paused = true;
+        }
+
+        @Override
+        public void stop()
+        {
+            started = false;
+            paused = false;
+            stopped = true;
+        }
+
+        @Override
+        public void restart()
+        {
+            reader.reset();
+            stopped = false;
+            started = false;
+            paused = false;
+        }
+
+        @Override
+        public boolean isStarted()
+        {
+            return started;
+        }
+
+        @Override
+        public boolean isPlaying()
+        {
+            return canProvide();
+        }
+
+        @Override
+        public boolean isPaused()
+        {
+            return paused;
+        }
+
+        @Override
+        public boolean isStopped()
+        {
+            return stopped;
+        }
+
+        @Override
+        public void setAudioSource(AudioInputStream inSource)
+        {
+            throw new UnsupportedOperationException("DCAPlayer does not use AudioInputStreams!");
+        }
+
+        @Override
+        public void setVolume(float volume)
+        {
+            throw new UnsupportedOperationException("Volume manipulation on DCA-streams is not possible!");
+        }
     }
 
-    private static class DCAReader
+    public static class DCAReader
     {
+        private int version;
         private JSONObject info;
         private InputStream inStr;
 
@@ -86,15 +181,16 @@ public class DCAUtil
                     } catch(IOException ignored) {}
                     throw new IllegalArgumentException("Provided Stream is not a DCA stream!");
                 }
-                int version = inStr.read() - '0';
-                System.out.println("Version: " + version);
+                version = inStr.read() - '0';
                 int length = inStr.read() | (inStr.read() << 8) | (inStr.read() << 16) | (inStr.read() << 24);
-                System.out.println("Length: " + length);
                 byte[] buff = new byte[length];
                 if(inStr.read(buff) != length)
                     throw new IOException("Could not write meta-block");
                 info = new JSONObject(new String(buff, StandardCharsets.UTF_8));
-                System.out.println(info.toString(4));
+                if (inStr.markSupported())
+                {
+                    inStr.mark(Integer.MAX_VALUE);
+                }
             }
             catch (IOException | JSONException e)
             {
@@ -102,6 +198,23 @@ public class DCAUtil
                 close();
                 throw e;
             }
+        }
+
+        public void reset()
+        {
+            if (inStr.markSupported())
+            {
+                try
+                {
+                    inStr.reset();
+                    return;
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            throw new UnsupportedOperationException("Given InputStream does not allow resets!");
         }
 
         public boolean atEnd() {
@@ -141,6 +254,16 @@ public class DCAUtil
                 } catch(IOException ignored) {}
                 inStr = null;
             }
+        }
+
+        public int getVersion()
+        {
+            return version;
+        }
+
+        public JSONObject getInfo()
+        {
+            return info;
         }
     }
 }
